72ef003e6401ab81d65d81fcd679c5e6c0faafb7
Adam Matteson <matteson211@hotmail.com>
2022-04-03 15:50:46 +0800

    navbar and masonry style ready
    :

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var colc = new Colcade('.grid', {
+  columns: '.grid-col',
+  items: '.grid-item'
+});
+
 
 const image = document.querySelectorAll('img')
 console.log(image)
@@ -8,3 +13,12 @@ image.forEach((pic) => {
     // pic.innerText="hi"
   });
 });
+
+
+// $('.grid').masonry({
+//   itemSelector: '.grid-item',
+//   columnWidth: 160,
+//   // percentPosition: true
+//   gutter: 10,
+//   fitWidth: true
+// });

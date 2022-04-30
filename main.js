733bd3fcc405928d7a411e6e112e003953bfd26e
Adam Matteson <matteson211@hotmail.com>
2022-04-30 16:30:54 +0800

    Made the basic page, but it still needs some massaging for the CSS on the individual project page. How to avoid duplicating code?

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+console.log('Are you receiving me?')
+// import Splide from '@splidejs/splide';
+
+
 var colc = new Colcade('.grid', {
   columns: '.grid-col',
   items: '.grid-item'
@@ -8,7 +12,6 @@ const images = document.querySelectorAll('img')
 images.forEach((pic) => {
   console.log(pic)
 
-
   pic.addEventListener('mouseover', (e) => {
     console.log(e, "youre hovering")
     // pic.innerText = title
@@ -18,10 +21,163 @@ images.forEach((pic) => {
 });
 
 
-// $('.grid').masonry({
-//   itemSelector: '.grid-item',
-//   columnWidth: 160,
-//   // percentPosition: true
-//   gutter: 10,
-//   fitWidth: true
+
+
+// new Splide('#image-carousel').mount();
+// document.addEventListener('DOMContentLoaded', function () {
+//   new Splide('#image-carousel').mount();
 // });
+
+
+
+
+// Tagged projects appear and disappear
+
+let comicButton = document.getElementById('comic');
+let drawingButton = document.getElementById('drawing');
+let artbookButton = document.getElementById('artbook');
+let printButton = document.getElementById('print');
+let showAllButton = document.getElementById('show-all');
+
+let comicPics = document.querySelectorAll('.comic');
+let drawingPics = document.querySelectorAll('.drawing');
+let artbookPics = document.querySelectorAll('.artbook');
+let printPics = document.querySelectorAll('.print');
+
+function showAll() {
+  comicPics.forEach((pic) => {
+    pic.style.display = 'block';
+  });
+  drawingPics.forEach((pic) => {
+    pic.style.display = 'block';
+  });
+  artbookPics.forEach((pic) => {
+    pic.style.display = 'block';
+  });
+  printPics.forEach((pic) => {
+    pic.style.display = 'block';
+  });
+  showAllButton.classList.add('active');
+  comicButton.classList.remove('active');
+  drawingButton.classList.remove('active');
+  artbookButton.classList.remove('active');
+  printButton.classList.remove('active');
+}
+
+function showComics() {
+  comicPics.forEach((pic) => {
+    pic.style.display = 'block';
+  });
+  drawingPics.forEach((pic) => {
+    pic.style.display = 'none';
+  });
+  artbookPics.forEach((pic) => {
+    pic.style.display = 'none';
+  });
+  printPics.forEach((pic) => {
+    pic.style.display = 'none';
+  });
+  showAllButton.classList.remove('active');
+  comicButton.classList.add('active');
+  drawingButton.classList.remove('active');
+  artbookButton.classList.remove('active');
+  printButton.classList.remove('active');
+}
+
+function showDrawings() {
+  comicPics.forEach((pic) => {
+    pic.style.display = 'none';
+  });
+  drawingPics.forEach((pic) => {
+    pic.style.display = 'block';
+  });
+  artbookPics.forEach((pic) => {
+    pic.style.display = 'none';
+  });
+  printPics.forEach((pic) => {
+    pic.style.display = 'none';
+  });
+  drawingButton.classList.add('active');
+  showAllButton.classList.remove('active');
+  comicButton.classList.remove('active');
+  artbookButton.classList.remove('active');
+  printButton.classList.remove('active');
+}
+
+function showArtbooks() {
+  comicPics.forEach((pic) => {
+    pic.style.display = 'none';
+  })
+  drawingPics.forEach((pic) => {
+    pic.style.display = 'none';
+  });
+  artbookPics.forEach((pic) => {
+    pic.style.display = 'block';
+  });
+  printPics.forEach((pic) => {
+    pic.style.display = 'none';
+  });
+  artbookButton.classList.add('active');
+  showAllButton.classList.remove('active');
+  comicButton.classList.remove('active');
+  drawingButton.classList.remove('active');
+  printButton.classList.remove('active');
+}
+
+function showPrints() {
+  comicPics.forEach((pic) => {
+    pic.style.display = 'none';
+  })
+  drawingPics.forEach((pic) => {
+    pic.style.display = 'none';
+  });
+  artbookPics.forEach((pic) => {
+    pic.style.display = 'none';
+  });
+  printPics.forEach((pic) => {
+    pic.style.display = 'block';
+  });
+  printButton.classList.add('active');
+  showAllButton.classList.remove('active');
+  comicButton.classList.remove('active');
+  drawingButton.classList.remove('active');
+  artbookButton.classList.remove('active');
+}
+
+// show all work
+showAllButton.addEventListener('click', () => {
+  showAll();
+});
+
+// show and hide comics
+comicButton.addEventListener('click', () => {
+  showComics();
+  // location.refresh();
+
+  // console.log('clicked comic')
+  // console.log(comicPics)
+  // comicPics.forEach((pic) => {
+  //   if (pic.style.display === 'none') {
+  //     pic.style.display = 'block';
+  //   }
+  //   else {
+  //     pic.style.display = 'none'
+  //   }
+  // });
+});
+
+// show and hide drawings
+drawingButton.addEventListener('click', () => {
+  showDrawings();
+
+});
+
+// show and hide artbook
+artbookButton.addEventListener('click', () => {
+  showArtbooks();
+});
+
+// show and hide prints
+printButton.addEventListener('click', () => {
+  showPrints();
+});
